b8df5ed3325b28671c5a7286f59ac1452ef2ba58
Martin Mendez <gmmendezp@gmail.com>
2017-06-17 16:44:25 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Nyssa BE Generator
+
+## Usage
+
+First, install [Yeoman](http://yeoman.io) and generator-nyssa-be using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo generator-nyssa-be
+```
+
+## Templates
+
+### Project
+
+To generate a new project:
+
+```bash
+yo nyssa-be
+```
+
+##### Options:
+
+- `name`
+  Name for the generated project. The default value is `FeathersTest`.
+- `path`
+  Relative path were the project will be generated, the generator will create the folders as needed. The default value is `.` (Current folder).
---
+++ generators/app/templates/src/app.test.js
@@ -0,0 +1,40 @@
+/* eslint-env jest */
+const rp = require('request-promise')
+const app = require('./app')
+
+describe('application tests', () => {
+  beforeAll(function (done) {
+    this.server = app.listen(3030)
+    this.server.once('listening', () => done())
+  })
+
+  afterAll(function (done) {
+    this.server.close(done)
+  })
+
+  describe('404', function () {
+    test('shows a 404 HTML page', () => {
+      return rp({
+        url: 'http://localhost:3030/path/to/nowhere',
+        headers: {
+          Accept: 'text/html'
+        }
+      }).catch(res => {
+        expect(res.statusCode).toBe(404)
+        expect(res.error.indexOf('<html>')).not.toBe(-1)
+      })
+    })
+
+    test('shows a 404 JSON error without stack trace', () => {
+      return rp({
+        url: 'http://localhost:3030/path/to/nowhere',
+        json: true
+      }).catch(res => {
+        expect(res.statusCode).toBe(404)
+        expect(res.error.code).toBe(404)
+        expect(res.error.message).toBe('Page not found')
+        expect(res.error.name).toBe('NotFound')
+      })
+    })
+  })
+})
